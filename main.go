f4d7c25f7830614271bff1efc345e916ead682ee
Anderson Souza <andersom.eugenio@gmail.com>
2019-09-12 18:08:31 -0300

    quebra de linha nos print de memoria

--- main.go
+++ main.go
@@ -159,12 +159,12 @@ func PrintMemUsage() {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("---------------------\n")
+	fmt.Printf("\n\n---------------------\n")
 	fmt.Printf("Alloc = %v MiB\n", bToMb(m.Alloc))
 	fmt.Printf("TotalAlloc = %v MiB\n", bToMb(m.TotalAlloc))
 	fmt.Printf("Sys = %v MiB\n", bToMb(m.Sys))
 	fmt.Printf("NumGC = %v\n", m.NumGC)
-	fmt.Printf("---------------------\n")
+	fmt.Printf("---------------------\n\n")
 }
 
 func bToMb(b uint64) uint64 {
